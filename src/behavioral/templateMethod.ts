7842df729e7b61c7adea55ac4d940efb227adc4d
Evgenii Davydov <jason0895@icloud.com>
2024-08-15 16:45:29 +0200

    implemented template method

---
+++ src/behavioral/templateMethod.ts
@@ -0,0 +1,43 @@
+abstract class Editor {
+    protected content: string;
+    protected isEditMode: boolean = false;
+    public open(): void {
+        this.isEditMode = true;
+    };
+    abstract edit(): void;
+    public close(): void {
+        this.isEditMode = false;
+        console.log(this.content);
+    }
+}
+class EditDate extends Editor {
+    public edit() {
+        if (!this.isEditMode) {
+            console.error('File is not opened');
+            return;
+        }
+        const day = new Date().getDate();
+        const month = new Date().getMonth() + 1;
+        const year = new Date().getFullYear();
+        this.content = `${day}.${month}.${year}`;
+    }
+}
+class EditTime extends Editor {
+    public edit() {
+        if (!this.isEditMode) {
+            console.error('File is not opened');
+            return;
+        }
+        const hours = new Date().getHours();
+        const minutes = new Date().getMinutes();
+        this.content = `${hours}:${minutes}`;
+    }
+}
+const editDate = new EditDate();
+const editTime = new EditTime();
+editDate.open();
+editDate.edit();
+editDate.close();
+editTime.open();
+editTime.edit();
+editTime.close();
